9534742b959c90b2f9a2c1e5fdb833ee933b3ff6
Miguelito1129 <mdmm1100@gmail.com>
2023-03-28 01:37:54 -0500

    first commit

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,45 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.util.List;
-
-
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity
 {
     private EditText editTextA;
-
-    private TextView textViewResultado;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         editTextA = findViewById(R.id.TextA);
-
-        textViewResultado = findViewById(R.id.Resultado);
-
-
-
-
     }
-    public void Fibonacci(View view)
+
+
+    public void Boton(View view)
     {
-        Fibonacci fibonacci = new Fibonacci();
         int numero;
-        String resultado;
         numero= Integer.parseInt(editTextA.getText().toString());
-        resultado = String.valueOf(fibonacci.calcularFibonacci(numero));
-        textViewResultado.setText(resultado+"");
+        if (numero == 3)
+        {
+            Intent intent = new Intent(this, Ejemplo.class);
+            startActivity(intent);
+        }
+        if (numero==1)
+        {
+            Intent intent = new Intent(this, Sumar.class);
+            startActivity(intent);
+        }
+        if (numero==2)
+        {
+            Intent intent = new Intent(this, Restar.class);
+            startActivity(intent);
+        }
+        if (numero==4)
+        {
+            Intent intent = new Intent(this, Multiplicar.class);
+            startActivity(intent);
+        }
     }
 }
